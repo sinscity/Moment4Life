4718b40679e1c82896ab36807dd2665a48c04f17
maraj <84688078+sinscity@users.noreply.github.com>
2022-09-19 02:17:29 -0400

    Update gettotop.lua

--- gettotop.lua
+++ gettotop.lua
@@ -2,7 +2,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 
 local Window = Library.CreateLib("Get to the Top", "GrapeTheme")
 local Win = Window:NewTab("Win")
-local Section = Win:NewSection("Telports")
+local Section = Win:NewSection("Teleports")
 Section:NewButton("Teleport to top section", "ButtonInfo", function()
     print("Teleported")
     local plyr = game.Players.LocalPlayer.Character
@@ -10,9 +10,13 @@ local Top = game:GetService("Workspace").TowerPosition.section12
 
 plyr.HumanoidRootPart.CFrame = Top.CFrame
 end)
+Section:NewButton("Infinite Yield", "ButtonInfo", function()
+    print("Admin loaded")
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
 
 local Credits = Window:NewTab("Credits")
-local Section = Credits:NewSection("Scripter: tink")
+local Section = Credits:NewSection("Credits: Tink")
 Section:NewKeybind("Toggle UI", "KeybindInfo", Enum.KeyCode.F, function()
 	Library:ToggleUI()
 end)
